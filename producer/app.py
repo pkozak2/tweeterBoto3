15f521b871a8a5f2d293e75980fa11a680c6e2ad
root <root@ip-172-31-21-2.eu-central-1.compute.internal>
2017-04-26 14:02:50 +0000

    Working with queue. Killer :)

--- producer/app.py
+++ producer/app.py
@@ -1,10 +1,22 @@
-import json, boto3, uuid
+import json, boto3, uuid, time
 sqs = boto3.resource('sqs', region_name='eu-central-1')
+counter = 0
+sendlatency = 0
 
 tweets = sqs.get_queue_by_name(QueueName='pkozak')
-response = tweets.send_message(MessageBody='Hello World', MessageAttributes={
-    'Author': {
-        'StringValue': 'Piotr',
-        'DataType': 'String'
-    }
-})
+
+while True:
+ counterstr = str(counter)
+ response = tweets.send_message(MessageBody='Message no:' + counterstr, MessageAttributes={
+      'Author': {
+          'StringValue': 'Piotr',
+          'DataType': 'String'
+      }
+  })
+ counter += 1
+ sendlatency += 1
+ if sendlatency == 50:
+    print("waiting");
+    time.sleep(15)
+    sendlatency = 0
+
